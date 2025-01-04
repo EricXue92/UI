750e9af87baf5198fa07cd59efaaebce38b938e3
Xue <eric@Xues-MacBook-Pro.local>
2025-01-04 11:35:34 +0800

    sycn

--- data_setup.py
+++ data_setup.py
@@ -1,10 +1,67 @@
+import os
+import requests
+import zipfile
+from pathlib import Path
 
-from wilds.common.data_loaders import get_train_loader
-import torchvision.transforms as transforms
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+NUM_WORKERS = os.cpu_count()
+
+# Setup path to data folder
+data_path = Path("data/")
+image_path = data_path / "pizza_steak_sushi"
+
+# If the image folder doesn't exist, download it and prepare it...
+if image_path.is_dir():
+    print(f"{image_path} directory exists.")
+else:
+    print(f"Did not find {image_path} directory, creating one...")
+    image_path.mkdir(parents=True, exist_ok=True)
+
+# Download pizza, steak, sushi data
+with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
+    request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
+    print("Downloading pizza, steak, sushi data...")
+    f.write(request.content)
+
+# Unzip pizza, steak, sushi data
+with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref:
+    print("Unzipping pizza, steak, sushi data...")
+    zip_ref.extractall(image_path)
+
+# Remove zip file
+os.remove(data_path / "pizza_steak_sushi.zip")
 
 
+def create_dataloaders(
+    train_dir: str,
+    test_dir: str,
+    transform: transforms.Compose,
+    batch_size: int,
+    num_workers: int=NUM_WORKERS
+):
+    # Use ImageFolder to create dataset(s)
+    train_data = datasets.ImageFolder(train_dir, transform=transform)
+    test_data = datasets.ImageFolder(test_dir, transform=transform)
 
+    # Get class names
+    class_names = train_data.classes
 
+    # Turn images into data loaders
+    train_dataloader = DataLoader(
+        train_data,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    test_dataloader = DataLoader(
+        test_data,
+        batch_size=batch_size,
+        shuffle=False,  # don't need to shuffle test data
+        num_workers=num_workers,
+        pin_memory=True,
+    )
 
-
-
+    return train_dataloader, test_dataloader, class_names
--- model_builder.py
+++ model_builder.py
@@ -1,70 +1,70 @@
-
 import torch
 import torch.nn as nn
 import torchvision
-import torch.nn.functional as F
-
 from sngp_wrapper.covert_utils import replace_layer_with_gaussian, convert_to_sn_my
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-num_gpus = torch.cuda.device_count()
 
 class ConvNextGP(nn.Module):
     def __init__(self, num_classes: int):
         super(ConvNextGP, self).__init__()
-        self.model_ft = torchvision.models.convnext_tiny(weights="ConvNeXt_Tiny_Weights.DEFAULT").to(device)
-        # Freeze the feature extraction layers
-        # for param in model.features.parameters():
-        #     param.requires_grad = False
-        self.num_ftrs = self.model_ft.classifier[2].in_features
-        self.model_ft.classifier = nn.Sequential(
-            nn.Linear(self.num_ftrs, 512),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(256, num_classes)
-        ).to(device)
-
+        feature_extractor = torchvision.models.convnext_tiny(weights="ConvNeXt_Tiny_Weights.DEFAULT")
+        feature_extractor.classifier = nn.Identity()
+        self.feature_extractor = feature_extractor
+        self.flatten = nn.Flatten(start_dim=1, end_dim=-1)
+        self.classifier = nn.Linear(768, num_classes)
     def forward(self, x, **kwargs):
-        # Forward pass through the feature extractor
-        x = self.model_ft.features(x)
-        # Global average pooling
-        x = self.model_ft.avgpool(x)
-        # Flatten the tensor
-        x = torch.flatten(x, 1)
-        # Pass through the custom classifier
-        x = self.model_ft.classifier(x)
-        return x
+        features = self.flatten(self.feature_extractor(x))
+        output = self.classifier(features, **kwargs)
+        return output
 
-
-def build_model(args, num_classes):
-    if args.sngp:
-        model = ConvNextGP(num_classes=num_classes)
-        GP_KWARGS = {
-            'num_inducing': 256,
-            'gp_scale': 1.0,
-            'gp_bias': 0.,
-            'gp_kernel_type': 'gaussian', # linear
-            'gp_input_normalization': True,
-            'gp_cov_discount_factor': -1,
-            'gp_cov_ridge_penalty': 1.,
-            'gp_output_bias_trainable': False,
-            'gp_scale_random_features': False,
-            'gp_use_custom_random_features': True,
-            'gp_random_feature_type': 'orf',
-            'gp_output_imagenet_initializer': True,
-            'num_classes': num_classes,
-        }
-        model = convert_to_sn_my(model, args.spec_norm_replace_list, args.coeff)
-        replace_layer_with_gaussian(container=model, signature="classifier", **GP_KWARGS)
-    elif args.snn:
-        model = ConvNextGP(num_classes=num_classes)
-    else:
-        raise ValueError("Invalid model type")
-    model = model.to(device)
+def build_model(num_classes):
+    model = ConvNextGP(num_classes=num_classes)
+    GP_KWARGS = {
+        'num_inducing': 1024,
+        'gp_scale': 1.0,
+        'gp_bias': 0.,
+        'gp_kernel_type': 'gaussian', # linear
+        'gp_input_normalization': True,
+        'gp_cov_discount_factor': -1,
+        'gp_cov_ridge_penalty': 1.,
+        'gp_output_bias_trainable': False,
+        'gp_scale_random_features': False,
+        'gp_use_custom_random_features': True,
+        'gp_random_feature_type': 'orf',
+        'gp_output_imagenet_initializer': True,
+        'num_classes': num_classes,
+    }
+    spec_norm_replace_list, coeff = ["Linear", "Conv2D"], 3.
+    model = convert_to_sn_my(model, spec_norm_replace_list, coeff)
+    replace_layer_with_gaussian(container=model, signature="classifier", **GP_KWARGS)
     return model
 
+def main():
+    num_classes = 10
+    kwargs = {"return_random_features": False, "return_covariance":False,
+              "update_precision_matrix": False, "update_covariance_matrix": False }
+
+    model = build_model(num_classes).to(device)
+    ind_data = torch.randn(10, 3, 224, 224).to(device)
+    ood_data = (torch.randn(10, 3, 224, 224) + 10).to(device)
+
+    for _ in range(10):
+        model(ind_data, **{"update_precision_matrix": True})  # we remember the in-domain data
+
+    model.classifier.update_covariance_matrix()
+
+    ind_output = model(ind_data, **{"update_precision_matrix": False, "return_covariance": True, })
+    ood_output = model(ood_data, **{"update_precision_matrix": False, "return_covariance": True, })
+
+    ind_prob, ind_cov = ind_output
+    ood_prob, ood_cov = ood_output
+
+    ind_uncertainty = torch.diagonal(ind_cov, 0)
+    ood_uncertainty = torch.diagonal(ood_cov, 0)
+
+    print("ind_uncertainty", ind_uncertainty, "ind mean", torch.mean(ind_uncertainty))
+    print("ood_uncertainty", ood_uncertainty, "ood mean", torch.mean(ood_uncertainty))
 
 
+if __name__ == "__main__":
+    main()
